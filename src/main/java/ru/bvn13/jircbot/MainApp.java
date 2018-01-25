cb685de46bd18b51400f707b77a077bf56fc8925
Vyacheslav N. Boyko <mail4bvn@gmail.com>
2018-01-25 14:05:37 +0300

    tested working as a daemon. you must place config.json near .jar file

--- src/main/java/ru/bvn13/jircbot/MainApp.java
+++ src/main/java/ru/bvn13/jircbot/MainApp.java
@@ -1,14 +1,17 @@
 package ru.bvn13.jircbot;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import ru.bvn13.jircbot.bot.JircBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @ComponentScan("ru.bvn13.jircbot")
-public class MainApp {
+public class MainApp implements CommandLineRunner {
 
     @Autowired
     private JircBot bot;
@@ -18,4 +21,8 @@ public class MainApp {
         System.out.println("==============> STARTING <==============");
     }
 
+    @Override
+    public void run(String... strings) throws Exception {
+
+    }
 }
