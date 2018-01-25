247bbf9746d148155296df299b2ae5f20ab0918a
Vyacheslav N. Boyko <mail4bvn@gmail.com>
2018-01-25 12:41:36 +0300

    bug fixed

--- src/main/java/ru/bvn13/jircbot/model/Config.java
+++ src/main/java/ru/bvn13/jircbot/model/Config.java
@@ -1,6 +1,5 @@
 package ru.bvn13.jircbot.model;
 
-import com.sun.istack.internal.NotNull;
 import lombok.Data;
 
 import java.util.List;
@@ -8,16 +7,12 @@ import java.util.List;
 @Data
 public class Config {
 
-    @NotNull
     private Boolean enabled = false;
 
-    @NotNull
     private String server;
 
-    @NotNull
     private Integer port = 6667;
 
-    @NotNull
     private String botName;
 
     private List<String> channelsNames;
